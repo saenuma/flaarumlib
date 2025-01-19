46092244cb7e417cf573369909dc684f98fe4cbf
Bankole Ojo <ojobankole@gmail.com>
2025-01-19 21:33:18 +0100

    imported the flaarum lib from the old monolith flaarum project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/saenuma/flaarumlib
+
+go 1.23.5
+
+require github.com/pkg/errors v0.9.1
---
+++ structs.go
@@ -0,0 +1,45 @@
+package flaarumlib
+
+type FieldStruct struct {
+	FieldName  string
+	FieldType  string
+	Required   bool
+	Unique     bool
+	NotIndexed bool
+}
+
+type FKeyStruct struct {
+	FieldName    string
+	PointedTable string
+	OnDelete     string // expects one of "on_delete_restrict", "on_delete_delete"
+}
+
+type TableStruct struct {
+	TableName   string
+	Fields      []FieldStruct
+	ForeignKeys []FKeyStruct
+}
+
+type WhereStruct struct {
+	FieldName   string
+	Relation    string // eg. '=', '!=', '<', etc.
+	FieldValue  string
+	Joiner      string   // one of 'and', 'or'
+	FieldValues []string // for 'in' and 'nin' queries
+}
+
+type StmtStruct struct {
+	TableName      string
+	Fields         []string
+	Expand         bool
+	Distinct       bool
+	StartIndex     int64
+	Limit          int64
+	OrderBy        string
+	OrderDirection string // one of 'asc' or 'desc'
+	Multi          bool
+	// variable parts
+	WhereOptions      []WhereStruct
+	MultiWhereOptions [][]WhereStruct
+	Joiner            string
+}
